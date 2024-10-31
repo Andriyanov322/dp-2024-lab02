13a9e627aa94a9f9fc5434618a4ce22efef1b06c
dfad <dfad>
2024-10-31 12:14:23 +0500

    fix(test_logger): Изменение работы тестов через метод AssertEqual

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -11,29 +11,29 @@ class TestLogger(unittest.TestCase):
         self.console_logger = Logger(writer=ConsoleWriter())
 
     def _build_expected_message(self, level, message):
-        """Формирует регулярное выражение для проверки отформатированного сообщения."""
-        return re.compile(rf"\d{{4}}-\d{{2}}-\d{{2}} \d{{2}}:\d{{2}}:\d{{2}} \[{level}\] {message}")
+        """Формирует отформатированное сообщение для проверки с квадратными скобками вокруг уровня."""
+        return f"[{level}] {message}"
 
     def test_log_info_console(self):
         with patch('builtins.print') as mock_print:
             self.console_logger.log("Test info message", level=LogLevel.INFO)
             expected_message = self._build_expected_message("INFO", "Test info message")
-            actual_message = mock_print.call_args[0][0]
-            self.assertRegex(actual_message, expected_message)
+            actual_message = re.sub(r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} ', '', mock_print.call_args[0][0]).strip()
+            self.assertEqual(actual_message, expected_message)
 
     def test_log_warning_console(self):
         with patch('builtins.print') as mock_print:
             self.console_logger.log("Test warning message", level=LogLevel.WARN)
             expected_message = self._build_expected_message("WARN", "Test warning message")
-            actual_message = mock_print.call_args[0][0]
-            self.assertRegex(actual_message, expected_message)
+            actual_message = re.sub(r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} ', '', mock_print.call_args[0][0]).strip()
+            self.assertEqual(actual_message, expected_message)
 
     def test_log_error_console(self):
         with patch('builtins.print') as mock_print:
             self.console_logger.log("Test error message", level=LogLevel.ERROR)
             expected_message = self._build_expected_message("ERROR", "Test error message")
-            actual_message = mock_print.call_args[0][0]
-            self.assertRegex(actual_message, expected_message)
+            actual_message = re.sub(r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} ', '', mock_print.call_args[0][0]).strip()
+            self.assertEqual(actual_message, expected_message)
 
     @patch("os.makedirs")
     @patch("builtins.open", new_callable=mock_open)
@@ -43,8 +43,8 @@ class TestLogger(unittest.TestCase):
         file_logger.log("Test info message", level=LogLevel.INFO)
 
         expected_message = self._build_expected_message("INFO", "Test info message")
-        actual_message = mock_open().write.call_args[0][0]
-        self.assertRegex(actual_message, expected_message)
+        actual_message = re.sub(r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} ', '', mock_open().write.call_args[0][0]).strip()
+        self.assertEqual(actual_message, expected_message)
 
     @patch("os.makedirs")
     @patch("builtins.open", new_callable=mock_open)
@@ -54,19 +54,21 @@ class TestLogger(unittest.TestCase):
         file_logger.log("Test warning message", level=LogLevel.WARN)
 
         expected_message = self._build_expected_message("WARN", "Test warning message")
-        actual_message = mock_open().write.call_args[0][0]
-        self.assertRegex(actual_message, expected_message)
+        actual_message = re.sub(r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} ', '', mock_open().write.call_args[0][0]).strip()
+        self.assertEqual(actual_message, expected_message)
 
     @patch("os.makedirs")
     @patch("builtins.open", new_callable=mock_open)
     def test_log_error_upper_file(self, mock_open, mock_makedirs):
         """Тестирование записи сообщения уровня ERROR в файл с преобразованием в верхний регистр."""
         upper_file_logger = Logger(writer=UpperFileWriter(directory="test_logs"))
-        upper_file_logger.log("Test error message", level=LogLevel.ERROR)
+        msg = 'Test error message'
 
-        expected_message = self._build_expected_message("ERROR", "TEST ERROR MESSAGE")
-        actual_message = mock_open().write.call_args[0][0]
-        self.assertRegex(actual_message, expected_message)
+        upper_file_logger.log(msg, level=LogLevel.ERROR)
+
+        expected_message = self._build_expected_message("ERROR", msg.upper())
+        actual_message = re.sub(r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} ', '', mock_open().write.call_args[0][0]).strip()
+        self.assertEqual(actual_message, expected_message)
 
     @patch("os.makedirs")
     @patch("builtins.open", new_callable=mock_open)
@@ -77,8 +79,10 @@ class TestLogger(unittest.TestCase):
         logger.log("Test info message in file", level=LogLevel.INFO)
 
         expected_message = self._build_expected_message("INFO", "Test info message in file")
-        actual_message = mock_open().write.call_args[0][0]
-        self.assertRegex(actual_message, expected_message)
+        actual_message = re.sub(r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} ', '', mock_open().write.call_args[0][0]).strip()
+        print(actual_message)
+        print(expected_message)
+        self.assertEqual(actual_message, expected_message)
 
 
 if __name__ == '__main__':
